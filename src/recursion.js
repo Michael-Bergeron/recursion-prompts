b795418711eabeec1b98a3f7ebbcff3389990e5d
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-05-28 16:09:39 -0500

    working on prompts in pairings

--- src/recursion.js
+++ src/recursion.js
@@ -7,33 +7,79 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  //if 0 or 1, return 1
+  if (n === 0 || n ===1){
+    return 1;
+  }
+  if (n < 0){
+    return null;
+  }
+  //return factorial function n(n-1)
+  return n * factorial(n-1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+
+  //if array is empty, return 0
+  // return pop last item
+  if (array.length === 0){
+    return 0;
+  }
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0){
+    return 0;
+  }
+  if (Array.isArray(array[0])){
+    return arraySum(array[0]) + arraySum(array.slice(1));
+  }
+  return array[0] + arraySum(array.slice(1));
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0){
+    return true;
+  }
+  if (n === 1){
+    return false;
+  }
+  return isEven(Math.abs(n)-2);
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n === 0){
+    return 0;
+  }
+  return n < 0 ? ((n + 1) + sumBelow(n + 1)) : ((n - 1) + sumBelow(n - 1));
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (x > y){
+    if (x - 1 === y || x === y){
+      return [];
+    }
+    return [(x-1), ...range((x - 1), y)];
+  }
+  if (x < y ){
+    if (x + 1 === y|| x === y){
+      return [];
+    }
+    return [(x+1), ...range((x + 1), y)];
+  }
+  return [];
 };
-
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
